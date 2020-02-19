f759fc67ffb61951917a555daaa4ec175157b9e3
Kalam Ahmed <kalamahmed714@gmail.com>
2020-02-19 10:55:09 +0600

    submit form

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <div class="holder">
+        <form @submit.prevent="addSkill">
+            <input type="text" placeholder="Enter your skill" v-model="skill" />
+            {{skill}}
+        </form>
         <ul>
             <li v-for="(data,index) in skills" :key="index">{{data.skill}}</li>
         </ul>
@@ -17,10 +21,18 @@ export default {
   name: 'Skills',
         data() {
             return {
+                skill:'',
                 skills: [
                     { 'skill': 'vue js' },
                     {'skill':'frontend developer'}
                 ]
+            }
+        },
+        methods: {
+            addSkill() {
+                this.skills.push({ skill: this.skill })
+                this.skill=''
+
             }
         }
 }
@@ -55,5 +67,13 @@ export default {
 
     .container {
         box-shadow: 0px 0px 40px lightgray;
+    }
+    input {
+        width: calc(100% - 40px);
+        border: 0;
+        padding: 20px;
+        font-size: 1.3em;
+        background-color: #323333;
+        color: #687F7F;
     }
 </style>
