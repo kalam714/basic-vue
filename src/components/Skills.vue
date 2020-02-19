f9b7c3de8245d7c87a8e2fd0a85312ff5c991224
Kalam Ahmed <kalamahmed714@gmail.com>
2020-02-19 10:39:20 +0600

    insert style

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="hello">
-    <h1>Skills</h1>
+    <div class="holder">
+        <ul>
+            <li v-for="(data,index) in skills" :key="index">{{data.skill}}</li>
+        </ul>
+        <p v-if="skills.length >1">You have more than one skills</p>
+        <p v-else>You have l or less than 1 skill</p>
+
+    </div>
     
   </div>
 </template>
@@ -8,26 +15,45 @@
 <script>
 export default {
   name: 'Skills',
-  props: {
-   
-  }
+        data() {
+            return {
+                skills: [
+                    { 'skill': 'vue js' },
+                    {'skill':'frontend developer'}
+                ]
+            }
+        }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    .holder {
+        background: #fff;
+    }
+
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
+
+        ul li {
+            padding: 20px;
+            font-size: 1.3em;
+            background-color: #E0EDF4;
+            border-left: 5px solid #3EB3F6;
+            margin-bottom: 2px;
+            color: #3E5252;
+        }
+
+    p {
+        text-align: center;
+        padding: 30px 0;
+        color: gray;
+    }
+
+    .container {
+        box-shadow: 0px 0px 40px lightgray;
+    }
 </style>
